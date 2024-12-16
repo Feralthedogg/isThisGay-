5288a84d183831aa75aa8535ea602d7e4c59fa1e
Feralthedogg <153323053+Feralthedogg@users.noreply.github.com>
2024-12-16 16:44:56 +0900

    Update isthisgay.js

--- isthisgay.js
+++ isthisgay.js
@@ -1,24 +1,23 @@
 const gayWords = [
-    "gay", // English, Spanish, French, Portuguese, Italian, Swedish, Norwegian, Danish, Indonesian, Tagalog, etc.
-    "게이", // Korean
-    "schwul", // German
-    "同性恋", // Chinese (Simplified)
-    "ゲイ", // Japanese
-    "гей", // Russian, Ukrainian, Bulgarian
-    "gej", // Serbian, Bosnian, Croatian
-    "homoseksual", // Malay
-    "γκέι", // Greek
-    "géi", // Hungarian
-    "gėjus", // Lithuanian
-    "گے", // Urdu, Pashto
-    "گی", // Dari
-    "गे", // Hindi, Marathi, Nepali
-    "gayu", // Albanian
-    "shoga", // Zulu
-    "gey", // Turkish, Azerbaijani
-    "גיי", // Hebrew
-    "gayu", // Georgian
-    "同性愛者" // Chinese (Traditional)
+    "gay", "hii kɛ hii", "abagaala ebisiyaga", "gai", "gay rehegua", "同性戀", "ગે", "ομοφυλόφιλος",
+    "homoseksuelli", "mosodoma", "isitabane", "homo", "समलिङ्गी", "homofil", "همجنسگرا",
+    "homoseksuel", "समलैंगिक", "schwul", "mulombwana", "cɛɲɔgɔnɲini", "ގޭ އެވެ", "moc",
+    "ເກ", "gejs", "гей", "gay a ni", "ng'ama chwo", "homosexuell", "umusambanyi", "abaryamana bahuje ibitsina",
+    "gėjus", "समलिंगी", "takatāpui", "геј", "pelaka", "സ്വവർഗ്ഗാനുരാഗി", "ڬاي", "gay;", "omosesswali",
+    "ижил хүйстэн", "bian nin bla nna nga be fa fite nzra nun", "ہم جنس پرست", "gayi", "ဂေး",
+    "đồng tính nam", "umulumendo", "সমকামী", "homofiila", "heñvelreizh", "bakla", "homosexuel",
+    "समलैङ्गिकः", "sọmọl", "ၵေႇ", "гаи", "bayot", "omoseksyel", "هاوڕەگەزباز", "khaniis", "ngochani",
+    "shoga", "gèidh", "homosexuales", "gej", "هم جنس پرست", "සමලිංගික", "مثلي الجنس", "գեյ",
+    "গে", "gay sat jaqiwa", "hommi", "masisi", "aerach", "gey", "gay, ɔbarima ne ɔbea nna",
+    "lacoo ma maro laco", "salaf xagole", "homoseksual", "ግብረ ሰዶማዊ", "Агеи", "gei", "geja",
+    "gayibɔ", "saalqunnamtii saala walfakkaataa raawwatu", "ସମଲିଙ୍ගୀ", "onibaje", "hoyw",
+    "ھەمجىنىسلار", "ߜ߭ߊߦߌ", "nwoke nwere mmasị nwoke", "פריילעך", "ゲイ", "გეი", "同性恋",
+    "omushaija omushaija", "Gay", "ಸಲಿಂಗಕಾಮಿ", "समलिंगी अशें म्हण्टात", "ខ្ទើយ", "Argaz",
+    "ⴰⵔⴳⴰⵣ", "ஓரின சேர்க்கையாளர்", "เกย์", "omoseksuál", "స్వలింగ సంపర్కుడు", "ge", "geý",
+    "ŵasepuka", "eşcinsel", "ግብረሰዶማዊ", "ཕོ་མོ་འདྲ་མཉམ།", "همجنسباز", "ਗੇ", "samkyndur",
+    "همجنس گرا", "homossexual", "sunnu-nyɔnu-nyɔnu", "wesoły", "gaynaako", "omosessuâl",
+    "tagane", "kāne kāne", "ɗan luwaɗi", "게이", "meleg", "समलैंगिक के बा", "homosexuël", "הומו",
+    "gay pipul dɛn"
 ];
 
 export function isThisGay(text) {
